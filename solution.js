c769d56c67e3fff24ac26a8a467d3db827e895ba
Azhar Kurnia <azharkurnia19@gmail.com>
2018-04-09 00:44:36 +0700

    written test done

--- solution.js
+++ solution.js
@@ -1,22 +1,25 @@
-const cheerio = require('cheerio');
-const rp = require('request-promise');
+// Created by Azhar Kurnia <azharkurnia19@gmail.com>
+// Last Modified: Monday, 09 April 2018 - 0:26 AM
+// Solution for Cermati Back-End Internship's programming test.
+
+"use strict";
+
+const cheerio = require("cheerio");
+const rp = require("request-promise");
+var fs = require("fs");
 
 const home_url = "https://m.bnizona.com/index.php/category/index/promo";
 var result = {};
-var halaman = 0;
 
 async function main() {
   try{
     let html = await rp(home_url);
     let $ = cheerio.load(html);
-    let data_home_page = $('div.arrow').toArray();
+    let data_home_page = $("div.arrow").toArray();
       for (var i = 0 ; i < data_home_page.length ; i++){
-        let category = data_home_page[i].next.next.children[0].data;
+        let category = data_home_page[i].next.next.children[0].data.toLowerCase();
         let category_url = data_home_page[i].next.next.attribs.href;
-        // console.log(category);
-        // console.log(i + category);
-        // console.log(i + category_url);
-
+      
         if (result[category]){
           result[category] = result[category];
         }
@@ -25,8 +28,7 @@ async function main() {
         }
         await access_category_page(category_url,category);
       }
-      // console.log(result);
-  
+      return result;  
 
   } catch (err){
     return console.log("ERROR DI main");
@@ -34,55 +36,55 @@ async function main() {
 }
 
 async function access_category_page(url,category){
-  // console.log("access category page parameter = "+ category);
   try{
+    let halaman = 0;
     let html = await rp(url);
     let $ = cheerio.load(html);
-    if ($('#loadmoreajaxloader').attr('href')){
-      let promo_url = $('#loadmoreajaxloader').attr('href')+"/"+halaman;
-      // console.log(promo_url);
-      access_promo_first_page(promo_url,category);
+    if ($("#loadmoreajaxloader").attr("href")){
+      let promo_url = $("#loadmoreajaxloader").attr("href")+"/";
+      access_promo(promo_url,category,halaman);
     }
   } catch (err){
-    return console.log("ERORR DI category");
+    return console.log(err);
   }
-  
 }
 
-async function access_promo_first_page(url,category){
-  // console.log("access promo page parameter = "+ category);
-
+async function access_promo(url,category,page){
   try{
-    let html = await rp(url);
+    let html = await rp(url+page);
     let $ = cheerio.load(html);
 
-    $('li > a').each(function(i, element){
-      let imageurl = $(this).children().attr('src');
+    $("li > a").each(function(i, element){
+      let imageurl = $(this).children().attr("src");
       let merchant = this.children[1].children[0].data;
       let title = this.children[2].children[0].data;
       let valid = this.children[3].children[0].data;
 
-
       var metadata = {
-        'imageurl' : imageurl,
-        'merchant' : merchant,
-        'title' : title,
-        'valid' : valid
+        "imageurl" : imageurl,
+        "merchant" : merchant,
+        "title" : title,
+        "valid" : valid.slice(12)
       };
       result[category].push(metadata);
-
     });
     
+    if (await rp(url+(page+10))){
+      access_promo(url,category,page+10);
+    }
     
   } catch (err){
-    return console.log("ERROR DI promo_first_page\n\n" + err);
+    return console.log("ERROR DI promo_first_page\n" + err);
   }
 }
 
 main()
-    .then(() => {
-        console.log('FINISHED');
-        console.log(result);
+    .then((data) => {
+        const json = JSON.stringify(data);
+        const filename = "solution.json"
+        fs.writeFileSync(filename,json);
+        console.log("File " + filename + " created.");
+        
     })
     .catch((e) => {
         console.log(e);
